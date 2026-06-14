ef174595f0e4ab05abf126323b977eef93aae099
tonystoneass <151034000+tonystoneass@users.noreply.github.com>
2026-06-14 16:04:15 +0200

    v2

---
+++ data/cities.js
@@ -0,0 +1,4 @@
+// Besuchte Orte/Städte – [Breitengrad, Längengrad, Name]
+var VISITED_CITIES = [
+	{ lat: 52.5200, lng: 13.4050, name: 'Berlin', date: '2024-05-10' }
+];
--- data/visited.js
+++ data/visited.js
@@ -1,2 +1,33 @@
 // Besuchte Länder – ISO Alpha-3 (z. B. DEU, FRA, USA)
-var VISITED_COUNTRIES = ['DEU', 'FRA', 'USA'];
+var VISITED_COUNTRIES = [
+    // --- EUROPA ---
+    'DEU', // Deutschland
+    'FRA', // Frankreich
+    'GRC', // Griechenland
+    'ITA', // Italien
+    'HRV', // Kroatien
+    'AUT', // Österreich
+    'CHE', // Schweiz
+    'SRB', // Serbien
+    'SVN', // Slowenien
+    'ESP', // Spanien
+    'CZE', // Tschechien
+    'HUN', // Ungarn
+    'GBR', // United Kingdom
+
+    // --- ASIEN ---
+    'IDN', // Indonesien
+    'ISR', // Israel
+    'OMN', // Oman
+    'SGP', // Singapur
+    'THA', // Thailand
+    'TUR', // Türkei
+    'ARE', // Vereinigte Arabische Emirate
+
+    // --- AFRIKA ---
+    'EGY', // Ägypten
+    'ZAF', // Südafrika
+
+    // --- NORDAMERIKA ---
+    'USA'  // USA
+];
